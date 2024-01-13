4b36e7d50d28cf6346b16647f0e2c72303edf8b4
marti <martiespinosa99@gmail.com>
2024-01-13 14:29:24 +0100

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Martí Espinosa Farran on 13/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
